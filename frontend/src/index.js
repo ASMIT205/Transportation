62ec8421ba0ff572765404378f71b6f9e62b4995
ASMIT205 <78749674+ASMIT205@users.noreply.github.com>
2024-11-13 19:32:07 +0530

    sixth commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import ServicesSection from './services';
+import ServicesSection from './Services';
 import reportWebVitals from './reportWebVitals';
 import Servicesbox from './services-box';
 import HeaderComponent from './AboutUs';
